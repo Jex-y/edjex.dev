135a589f965ebd2c57a5acc4a320e0ff28f9965d
Ed Jex <edwardjex@live.co.uk>
2025-07-18 22:34:42 +0100

    spelling + update github card

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'edjex.dev';
-export const SITE_DESCRIPTION = 'Ed Jex';
+export const SITE_TITLE = 'Ed Jex';
+export const SITE_DESCRIPTION = 'Ed Jex - Recently graduated top of my cohort from a masters in Computer Science at Durham University. I am currently working as a full stack developed at Solve Intelligence, building AI tools for the pattent application process.';
 export const SITE_URL = 'https://edjex.dev';
 export const GITHUB_URL = 'https://github.com/Jex-y';
